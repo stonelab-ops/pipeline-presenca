a7663c6f8c395222efd6fb1d2085aa33889ad2e0
Gabriel Canto <gabriel.canto@stone.com.br>
2026-01-29 10:54:39 -0300

    Config: Atualiza ID correto da Planilha Mestra (Google Sheets)

--- configs/settings_colab.py
+++ configs/settings_colab.py
@@ -6,8 +6,7 @@ CAMINHOS = {
     'colab': {
         'dados_presenca': "/gdrive/MyDrive/projetos-colab-compartilhados/Sistema_Gestao_Presenca/Database/Raw_unstructured_data/xml_biometria",
         'id_pasta_saida': "15MPcIJyU5xsf-vMDvw2Sa7a2KJZjoObt",
-        'ID_PLANILHA_MESTRA': "1twMbYDB9Na64iKIkCg-qfXxvd1QU8Kg0", 
-        'dashboard': 'output-dashboard',
+        'ID_PLANILHA_MESTRA': "1mXo2p-ennpprgn3JLXY83qOdDsN-mJXDk-qC2WTzp8I",        'dashboard': 'output-dashboard',
         'test_data': 'test_data'
     }
 }
